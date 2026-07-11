873309a09cffd9fb37d237e9ea92acd8a6950733
XukaCT <sangnguyen260920@gmail.com>
2026-07-11 20:57:49 +1000

    Using Postman to test sign up function and fix minor bugs

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,5 @@
-import bcrypt from "bcryptjs";
-import User from "../models/userModel.js";
+import bcrypt from "bcrypt";
+import User from "../models/User.js";
 
 export  const signup = async (req, res) => {
     try{
