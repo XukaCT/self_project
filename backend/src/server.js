e58fa6ccf3614d60912d1400eb390c1231bbcf18
XukaCT <sangnguyen260920@gmail.com>
2026-07-11 15:34:56 +1000

    Initialize backend with Express, MongoDB connection, and dotenv configuration

---
+++ backend/src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import {connectDB} from './libs/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+connectDB().then(() => {
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+})
+
+
